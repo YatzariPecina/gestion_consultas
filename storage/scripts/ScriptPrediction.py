b913c1701fd59b8fa78cdbc25f2e41975fda3316
Yatzari Pecina <2130039@example.com>
2024-08-12 20:01:32 -0600

    unos problemillas

--- storage/scripts/ScriptPrediction.py
+++ storage/scripts/ScriptPrediction.py
@@ -3,33 +3,36 @@ import joblib
 import json
 from sklearn import preprocessing
 
-try:
-    # Cargar el modelo pkl
-    with open("magic_wand.pkl", 'rb') as file:
-        modelo_scaler = joblib.load(file)
-        modelo = modelo_scaler['modelo']
-        scaler = modelo_scaler['scaler']
+def main():
+    try:
+        # Cargar el modelo pkl
+        with open("magic_wand.pkl", 'rb') as file:
+            modelo_scaler = joblib.load(file)
+            modelo = modelo_scaler['modelo']
+            scaler = modelo_scaler['scaler']
 
-    # Leer los datos de los argumentos de línea de comandos
-    if len(sys.argv) > 1:
-        input_data = json.loads(sys.argv[1])  # Decodifica el JSON recibido
+        # Leer los datos de los argumentos de línea de comandos
+        if len(sys.argv) > 1:
+            input_data = json.loads(sys.argv[1])  # Decodifica el JSON recibido
 
-        # Verifica que los datos se puedan transformar
-        if isinstance(input_data, list) and len(input_data) > 0:
-            nuevos_datos_standar = scaler.transform(input_data)
+            if isinstance(input_data, list) and len(input_data) > 0:
+                nuevos_datos_standar = scaler.transform(input_data)
 
-            # Hacer la predicción
-            prediction = modelo.predict(nuevos_datos_standar)
-            
-            # Mostrar la predicción en la consola
-            print(f"Predicción para los datos: {prediction}")
+                # Hacer la predicción
+                prediction = modelo.predict(nuevos_datos_standar)
+                
+                # Mostrar la predicción en la consola
+                print(f"Predicción para los datos: {prediction}")
+            else:
+                print("Datos no válidos para la transformación.")
+                sys.exit(1)  # Código de error
         else:
-            print("Datos no válidos para la transformación.")
+            print("No se recibieron datos.")
             sys.exit(1)  # Código de error
-    else:
-        print("No se recibieron datos.")
+
+    except Exception as e:
+        print(f"Error al procesar los datos: {e}")
         sys.exit(1)  # Código de error
 
-except Exception as e:
-    print(f"Error al procesar los datos: {e}")
-    sys.exit(1)  # Código de error
+if __name__ == "__main__":
+    main()
