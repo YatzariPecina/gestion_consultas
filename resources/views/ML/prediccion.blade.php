a3d6d5766adedd95d15a887c6b6876beb6935f68
DamarisEspinosa <2130081@upv.edu.mx>
2024-08-21 21:07:24 -0600

    pagina de prediccion

--- resources/views/ML/prediccion.blade.php
+++ resources/views/ML/prediccion.blade.php
@@ -5,26 +5,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prediccion</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-image: url('img/fondo.png');
+            background-size: cover; 
+            background-repeat: no-repeat; 
+            background-attachment: fixed; 
+            background-position: center;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .contenido {
+            padding: 100px;
+            border-radius: 25px;
+            background-color: rgba(255, 255, 255, 0.7); 
+        }
+        .titulo{
+            text-align: center;
+        }
+        .hechizo{
+            text-align: center;
+            font-size: 80px;
+            color: #b35422; /*#c96f3f*/
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Resultado de la predicción</h1>
-    <div class="my-4">
-        @session('success')
-            <div class="alert alert-success" role="alert">
-                {{ $value }}
-            </div>
-        @endsession
-    </div>
+    <div class="contenido">
+        <h1 class="titulo">Resultado de la predicción</h1>
+        <div class="my-5">
+            @session('success')
+                <div class="alert alert-success" role="alert">
+                    {{ $value }}
+                </div>
+            @endsession
+        </div>
 
-    <div>
-        @foreach ($data as $row)
-            <tr>
-                @foreach ($row as $cell)
-                    <td>{{ $cell }}</td>
-                @endforeach
-            </tr>
-        @endforeach
+        <div class="hechizo">
+            @foreach ($data as $row)
+                <tr>
+                    @foreach ($row as $cell) 
+                        <td>{{ $cell }}</td>
+                    @endforeach
+                </tr>
+            @endforeach
+        </div>
     </div>
 </body>
 
